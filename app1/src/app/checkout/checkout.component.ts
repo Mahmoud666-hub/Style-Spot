a92bbc87e5397dd794bb237d3059582183049a06
Mohamed Abdallah <Mhvvvv59@gmail.com>
2024-10-08 11:04:46 +0300

    MH

--- app1/src/app/checkout/checkout.component.ts
+++ app1/src/app/checkout/checkout.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent {
+
+  Country=["...","Egypt","America","Australia","Armania","Suadi","Oman","UAE"];
+
 
 }
